b7929db93f747a3d1f863650229fd324abb1826b
Marylinelesaffre <marylineetnoe@outlook.fr>
2024-10-21 16:28:00 +0200

    [WIP] javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let tour_eiffel;
+let notre_dame;
+let arc_de_triomphe;
+let mont_saint_michel;
+let versailles;
+let title = "";
+let pays = "";
+let info = "";
+
+
+function maFonction(monument) {
+
+
+if (monument === 'tour_eiffel') {
+    title = "Tour Eiffel";
+    pays = "Paris, France";
+    info = " La tour Eiffel est un monument emblématique de Paris, construit par Gustave Eiffel pour l'Exposition universelle de 1889"
+}else if(monument ==='notre_dame');
+
+}
+console.log("monument", title, "pays", pays, "info" );
+
